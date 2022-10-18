1bc82fe6219cf1c02bbd0a06ac4a529ef385cac9
Daniel-Sanchez-111 <62917581+Daniel-Sanchez-111@users.noreply.github.com>
2022-10-18 09:55:48 -0600

    controladores

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 
-
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
 
@@ -19,8 +19,14 @@ Route::get('/saludo/{name}', function ($name) {
 Route::get('/suma/{num1}/{num2}/{num3}?', function ($num1,$num2,$num3=0) {
     echo $num1+$num2+$num3;
 })->where(['num1','[0-9]+'],['num2','[0-9]+']);
-
-/*Route::post('suma/',function(Request $request)){
-
-}
 */
+
+
+Route::get('users/', [UserController::class, 'index']);
+
+Route::get('users/create', [UserController::class, 'create']);
+
+Route::get('users/{id}', [UserController::class, 'show']);
+
+Route::post('users/', [UserController::class, 'store']);
+
